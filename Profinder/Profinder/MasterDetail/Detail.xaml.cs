2c1617dca0558a65346c6a9693bc72c10d9e02fb
João Paulo Amaral <joaopaulofernandes@hotmail.com>
2020-08-28 19:15:39 -0400

    Adicionar arquivos de projeto.

---
+++ Profinder/Profinder/MasterDetail/Detail.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Profinder.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Profinder
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Detail : ContentPage
+    {
+        public Detail()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnFiltro_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Filtro());
+
+        }
+
+        private async void BtnFavorito_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Favoritos());
+        }
+
+        private async void BtnPesquisa_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Pesquisa());
+        }
+    }
+}
---
+++ Profinder/Profinder/MasterDetail/Master.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Profinder.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace Profinder
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : ContentPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+       
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+        }
+
+        private async void TapGestureRecognizer_Tapped_2(object sender, EventArgs e)
+        {
+            await App.NavigationMasterDetail(new Favoritos());
+        }
+
+        private async void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            await App.NavigationMasterDetail(new Login());
+        }
+    }
+
+}
